909875806f43e41c71b0ac38e1a2c7ea50f121f9
Akash Singh <furiousakash111@gmail.com>
2023-07-21 17:58:37 +0530

    Added add stocks section

--- Server/controllers/addStocks.js
+++ Server/controllers/addStocks.js
@@ -1,2 +1,24 @@
 const User = require('../models/clientSchema');
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const secret_key = process.env.SECRET;
+
+exports.pushStocks = async(req, res) => {
+    const {token, stocks} = req.body;
+    if(!token){
+        return res.status(401).json({
+            success: false,
+            message: "Token missing!!",
+        });
+    }
+    const decode = jwt.verify(token, secret_key);
+    const filter = {clientCode: decode.clientCode};
+    const update = {watchlist : stocks};
+    const updatedClient = await User.findOneAndUpdate(filter, update, {new:true});
+    return res.status(200).json({
+        success: true,
+        message: "Stocks added to watchlist successfully!!",
+        client: updatedClient,
+    })
+}
--- Server/routes/users.js
+++ Server/routes/users.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { login, signup } = require('../controllers/authN');
+const { pushStocks } = require('../controllers/addStocks');
 const router = express.Router();
 
 router.post('/login', login);
 router.post('/signup', signup);
+router.post('/addstocks', pushStocks);
 
 module.exports = router;
