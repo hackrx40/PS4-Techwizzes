089281793cd5832a1bbbeb77bf3d55097bbf92b7
Akash Singh <furiousakash111@gmail.com>
2023-07-22 09:49:29 +0530

    Historical charts updated

--- Server/controllers/historicalCharts.js
+++ Server/controllers/historicalCharts.js
@@ -1,20 +1,64 @@
-const HisPrice = require('../models/historicalPrice');
-const HisChart = require('../models/historicalChartModel');
+const HisPrice = require('../data/pastPrices.json');
 const ChartJsImage = require('chartjs-to-image');
 
-stocks_array = [
-    "BHARTIARTL", "POWERGRID", "ICICIBANK", "ASIANPAINT", "BRITANNIA", "INFY", "NTPC",
-    "CIPLA", "ONGC", "HDFCBANK", "SUNPHARMA", "EICHERMOT", "TECHM", "M&M", "AXISBANK",
-    "RELIANCE", "WIPRO", "HINDALCO", "HINDUNILVR", "SBILIFE", "ULTRACEMCO", "APOLLOHOSP",
-    "HDFC", "BAJAJFINSV", "COALINDIA", "DRREDDY", "ITC", "HEROMOTOCO", "DIVISLAB", "GRASIM",
-    "HCLTECH", "TCS", "UPL", "KOTAKBANK", "TATACONSUM", "INDUSINDBK", "JSWSTEEL", "LT", "SBIN",
-    "TATAMOTORS", "HDFCLIFE", "MARUTI", "BPCL", "TITAN", "BAJFINANCE", "ADANIPORTS", "NESTLEIND",
-    "TATASTEEL", "ADANIENT", "BAJAJ-AUTO"
-]
-
-
-const gethistory = async(req,res) => {
-    let hisprices = await HisPrice.find({});
-    console.log(hisprices);
+exports.setChartLink = async (req, res) => {
+    try {
+        const { symbol } = req.body;
+        let index = 0;
+        let links = [];
+        for (i = 0; i < 50; i++) {
+            if (HisPrice[i].symbol == symbol) {
+                index = i;
+                break;
+            }
+        }
+        let label = [];
+        let data = [];
+        let k = 0;
+        for (i = index; i < HisPrice.length; i += 50) {
+            label.push(k.toString());
+            k++;
+            data.push(parseFloat(HisPrice[i].close));
+        }
+        async function generateChart1w() {
+            const myChart = new ChartJsImage();
+            myChart.setConfig({
+                type: 'line',
+                data: { labels: label.slice(0, 7), datasets: [{ data: data.slice(0, 7), fill: false }] },
+            });
+            w1Url = await myChart.toDataUrl();
+            links.push(String(w1Url));
+        }
+        generateChart1w();
+        async function generateChart1m() {
+            const myChart = new ChartJsImage();
+            myChart.setConfig({
+                type: 'line',
+                data: { labels: label.slice(0, 20), datasets: [{ data: data.slice(0, 20), fill: false }] },
+            });
+            m1Url = await myChart.toDataUrl();
+            links.push(String(m1Url));
+        }
+        generateChart1m();
+        async function generateChart3m() {
+            const myChart = new ChartJsImage();
+            myChart.setConfig({
+                type: 'line',
+                data: { labels: label.slice(0, 60), datasets: [{ data: data.slice(0, 60), fill: false }] },
+            });
+            m3Url = await myChart.toDataUrl();
+            console.log(m3Url);
+            links.push(String(m3Url));
+        }
+        generateChart3m();
+        res.status(200).json({
+            success: true,
+            links: links,
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Something went wrong!!",
+        })
+    }
 }
-gethistory();
